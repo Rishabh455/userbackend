0c6ca1a2bffceff1a6a32864ac2ffd3901ccaab2
Rishabh455 <94906188+Rishabh455@users.noreply.github.com>
2023-04-02 15:34:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoDB();
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://userfrontend-seven.vercel.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin,X-Requested-With,Content-Type,Accepet"
